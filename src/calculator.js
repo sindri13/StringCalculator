b63709feaba1cda40bc365e52dd6f27ea9646928
Sindri B <sindri13@ru.is>
2018-10-12 16:56:10 +0000

    Added so that the add function can take in one number

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,8 @@
 function add (number) {
+if(number == "")
   return 0;
+
+  return parseInt(number);
 }
 
 module.exports = add; // Here we are exporting the add function so other programs can use it
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("./calculator"); // Including the calculator.js
 it("Should return a zero on an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+it("Should return the number input if it's only one number", () => {
+  expect(add("5")).toBe(5);
+});
